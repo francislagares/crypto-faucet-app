38695d17df02f9b02707fff9611d49f293d663a2
Francis Lagares <francis.lagares@gmail.com>
2021-12-01 20:29:09 +0100

    feat: first smart contract

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+  // storage variables
+  uint public funds = 1000; // positive values only
+  int public counter = -1;
+  uint32 public test = 4294967295;
+}
